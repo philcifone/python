ffca95c43926c7f4dbc9d2a36f7e885fa950b315
philcifone <philcif@gmail.com>
2023-07-09 14:36:35 -0400

    added progress bar!

--- pixel-art.py
+++ pixel-art.py
@@ -1,5 +1,6 @@
 import numpy as np
 from PIL import Image
+from tqdm import tqdm
 
 def pixel_art_generator(image_path, pixel_size, output_path):
     # Open the image
@@ -14,6 +15,9 @@ def pixel_art_generator(image_path, pixel_size, output_path):
     # Create a new blank image for the pixel art
     pixel_art = Image.new("RGB", (width, height))
 
+    # Initialize the progress bar
+    progress_bar = tqdm(total=new_width * new_height, desc="Generating Pixel Art", unit="pixel")
+
     # Iterate over each pixel in the resized image
     for x in range(new_width):
         for y in range(new_height):
@@ -25,6 +29,12 @@ def pixel_art_generator(image_path, pixel_size, output_path):
                 for j in range(pixel_size):
                     pixel_art.putpixel((x * pixel_size + i, y * pixel_size + j), pixel_color)
 
+            # Update the progress bar
+            progress_bar.update()
+
+    # Close the progress bar
+    progress_bar.close()
+
     # Save the pixel art image
     pixel_art.save(output_path)
 
@@ -32,8 +42,6 @@ def pixel_art_generator(image_path, pixel_size, output_path):
 image_path = input("Enter the path to the input image file: ")
 pixel_size = int(input("Enter the desired pixel size for the pixel art: "))
 output_path = input("Enter the output path and filename to save the pixel art image: ")
-
-print("working...")
 
 # Call the pixel art generator function
 pixel_art_generator(image_path, pixel_size, output_path)
